0716b4cf24857c0ac6a803a29883952c0c803df2
dhnpx <122553352+dhnpx@users.noreply.github.com>
2024-09-30 22:58:30 -0700

    fixes

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ mode=$1
 rm -rf ~/.config/nvim
 rm -rf ~/.config/hypr
 rm -rf ~/.config/waybar
+rm -rf ~/.config/foot
 rm -rf ~/.wezterm.lua
 rm -rf ~/.zshrc
 
@@ -18,6 +19,7 @@ fi
 ln -sf ~/.dotfiles/nvim ~/.config/nvim
 ln -sf ~/.dotfiles/waybar ~/.config/waybar
 ln -sf ~/.dotfiles/wezterm/.wezterm.lua ~/.wezterm.lua
+ln -sf ~/.dotfiles/foot ~/.config/foot
 ln -sf ~/.dotfiles/Yubico/u2f_keys ~/.config/Yubico/u2f_keys
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/p10k/.p10k.zsh ~/.p10k.zsh 
