d96a271f94596da0077def9e73a06068f885d2b6
dhnpx <122553352+dhnpx@users.noreply.github.com>
2025-05-31 05:28:28 -0700

    changed to rel file path instead of absolute. added network manager script to auto change time zone on internet connect

--- install.sh
+++ install.sh
@@ -20,17 +20,21 @@ else
 fi
 
 
-ln -sf ~/.dotfiles/nvim ~/.config/nvim
-ln -sf ~/.dotfiles/waybar ~/.config/waybar
-ln -sf ~/.dotfiles/wezterm/.wezterm.lua ~/.wezterm.lua
-ln -sf ~/.dotfiles/foot ~/.config/foot
-#ln -sf ~/.dotfiles/Yubico/u2f_keys ~/.config/Yubico/u2f_keys
-ln -sf ~/.dotfiles/.zshrc ~/.zshrc
-#ln -sf ~/.dotfiles/p10k/.p10k.zsh ~/.p10k.zsh 
-ln -sf ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -sf ~/.dotfiles/.spaceshiprc.zsh ~/.spaceshiprc.zsh
-ln -sf ~/.dotfiles/yazi ~/.config/yazi
-ln -sf ~/.dotfiles/ghostty ~/.config/ghostty
-ln -sf ~/.dotfiles/wofi ~/.config/wofi
+ln -sf ./nvim ~/.config/nvim
+ln -sf ./waybar ~/.config/waybar
+ln -sf ./wezterm/.wezterm.lua ~/.wezterm.lua
+ln -sf ./foot ~/.config/foot
+#ln -sf ./Yubico/u2f_keys ~/.config/Yubico/u2f_keys
+ln -sf ./.zshrc ~/.zshrc
+#ln -sf ./p10k/.p10k.zsh ~/.p10k.zsh 
+ln -sf ./tmux/.tmux.conf ~/.tmux.conf
+ln -sf ./.spaceshiprc.zsh ~/.spaceshiprc.zsh
+ln -sf ./yazi ~/.config/yazi
+ln -sf ./ghostty ~/.config/ghostty
+ln -sf ./wofi ~/.config/wofi
 
-sudo cp -r ~/.dotfiles/fonts/MapleMonoNL-NF-unhinted/ /usr/share/fonts/
+sudo cp -r ./fonts/MapleMonoNL-NF-unhinted/ /usr/share/fonts/
+
+chmod +x ./network_manager/09-timezone
+sudo cp ./network_manager/09-timezone /etc/NetworkManager/dispatcher.d/
+sudo chown root /etc/Networkmanager/dispatcher.d/09-timezone
