7c849a711c34d02eeb0cb5f780e3aef5e2b8403f
dhnpx <122553352+dhnpx@users.noreply.github.com>
2026-01-17 02:56:09 -0800

    changes to make.conf and install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ mode=$1
 rm -rf ~/.config/nvim
 rm -rf ~/.config/hypr
 rm -rf ~/.config/waybar
-rm -rf ~/.config/foot
-rm -rf ~/.wezterm.lua
+#rm -rf ~/.config/foot
+#rm -rf ~/.wezterm.lua
 rm -rf ~/.zshrc
-rm -rf ~/.spaceshiprc.zsh
+#rm -rf ~/.spaceshiprc.zsh
 rm -rf ~/.config/yazi
 rm -rf ~/.config/ghostty
 rm -rf ~/.config/wozi
@@ -26,18 +26,18 @@ fi
 
 ln -sf ~/.dotfiles/nvim ~/.config/nvim
 ln -sf ~/.dotfiles/waybar ~/.config/waybar
-ln -sf ~/.dotfiles/wezterm/.wezterm.lua ~/.wezterm.lua
-ln -sf ~/.dotfiles/foot ~/.config/foot
+#ln -sf ~/.dotfiles/wezterm/.wezterm.lua ~/.wezterm.lua
+#ln -sf ~/.dotfiles/foot ~/.config/foot
 #ln -sf ~/.dotfiles/Yubico/u2f_keys ~/.config/Yubico/u2f_keys
 ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/.dotfiles/zsh/.zsh_plugins.txt ~/.zsh_plugins.txt
+#ln -sf ~/.dotfiles/zsh/.zsh_plugins.txt ~/.zsh_plugins.txt
 #ln -sf ~/.dotfiles/p10k/.p10k.zsh ~/.p10k.zsh 
 ln -sf ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/tmux/tmux_sessionzier ~/bin/tmux_sessionizer
 ln -sf ~/.dotfiles/yazi ~/.config/yazi
 ln -sf ~/.dotfiles/ghostty ~/.config/ghostty
 ln -sf ~/.dotfiles/wofi ~/.config/wofi
-ln -sf ~/.dotfiles/hyprdynamicmonitors/ ~/.config/hyprdynamicmonitors
+#ln -sf ~/.dotfiles/hyprdynamicmonitors/ ~/.config/hyprdynamicmonitors
 
 sudo cp -r ./fonts/MapleMonoNL-NF-unhinted/ /usr/share/fonts/
 
